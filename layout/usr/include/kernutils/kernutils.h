40bdc876f7352dc5ad07b6db3c04d4e0d1ed9369
Tommy Muir <31448720+Muirey03@users.noreply.github.com>
2019-09-09 18:05:47 +0100

    Initial commit

---
+++ layout/usr/include/kernutils/kernutils.h
@@ -0,0 +1,13 @@
+#ifndef _KERNUTILS_H
+#define _KERNUTILS_H
+
+#include <kernutils/common.h>
+#include <kernutils/kern_utils.h>
+#include <kernutils/kexecute.h>
+#include <kernutils/kmem.h>
+#include <kernutils/offsetcache.h>
+#include <kernutils/offsetof.h>
+#include <kernutils/osobject.h>
+#include <kernutils/sandbox.h>
+
+#endif
